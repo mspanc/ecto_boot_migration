b91ec33ad42211a626ff7f3d37be2ac9183d0496
Marcin Lewandowski <marcin@radiokit.org>
2019-01-19 00:26:20 +0100

    Bumped to 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoBootMigration.Mixfile do
 
   def project do
     [app: :ecto_boot_migration,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
